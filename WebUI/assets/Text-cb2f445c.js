dbe4c59b33e076954c3539777a08ff8ad321b703
sanenchen <sanenchen@sanenchendeiMac.local>
2023-06-07 00:09:01 +0800

    移除又拍云图标，修复拖动窗口导航栏问题

--- WebUI/assets/Text-cb2f445c.js
+++
@@ -1,1 +0,0 @@
-import{r,e as o}from"./index-ffd77a69.js";import{u,a as l,r as n}from"./useTextStyles-29a546f5.js";const x=r.forwardRef((s,t)=>{const e=u(s,t);l(e);const{useTextStyles_unstable:a}=o();return a(e),n(e)});x.displayName="Text";export{x as T};
